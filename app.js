6dcfd2d9fbff4ff6202ff7eb7eb08c28a0f0defa
rokasandreikenas <shyptt@gmail.com>
2020-07-14 10:30:26 +0300

    add date format;add button for stories

--- app.js
+++ app.js
@@ -18,13 +18,28 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handlebars helpers
+const { formatDate } = require("./helpers/hbs");
 // Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: {
+      formatDate,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 // Session
@@ -47,6 +62,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
